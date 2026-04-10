60cffd89bf8ea661b07ccb9dbd96e552188c1c63
t0667 <nikhil.a@triassicsolutions.com>
2026-04-10 11:26:41 +0530

    prod structure purge based

--- app/utils/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.config
-from app.utils.logging_config import LOGGING_CONFIG
-
-logging.config.dictConfig(LOGGING_CONFIG)
-
-def get_logger(name: str = "app"):
-    return logging.getLogger(name)
